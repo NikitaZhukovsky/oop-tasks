6e85f46e4f74e570dfc82276f9dc77d2cb389528
NikitaZhukovsky <nikzuk2004@mail.ru>
2024-01-24 15:56:30 +0300

    New version

--- Task_5/super_str.py
+++ Task_5/super_str.py
@@ -2,19 +2,15 @@ class SuperStr(str):
     def is_repeatance(self, s):
         if self == '':
             return False
-        if len(self) % len(s) == 0 and (self[:len(s)] * (len(self) // len(s))) == self:
-            return True
-        return False
+        return (len(self) % len(s) == 0) and (s * (len(self) // len(s)) == self)
 
     def is_palindrom(self):
-        if self.lower() == self.lower()[::-1]:
-            return True
-        return False
+        return self.lower() == self.lower()[::-1]
 
 
-repeatance = SuperStr('abcabcabc')
+repeatance = SuperStr('abc')
 some_str = 'abc'
 print(f"Текущая строка {repeatance} может быть получена повторениями {some_str}: {repeatance.is_repeatance(some_str)}")
 
-palindrom = SuperStr('abccba')
+palindrom = SuperStr('Abccba')
 print(f"Строка является палиндромом: {palindrom.is_palindrom()}")
