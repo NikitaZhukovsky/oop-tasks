7ecdaf9cff8af2a01ef9364b1f3f9252f708e0d1
NikitaZhukovsky <nikzuk2004@mail.ru>
2024-01-24 15:07:24 +0300

    New version

--- Task_1/soda.py
+++ Task_1/soda.py
@@ -3,10 +3,7 @@ class Soda:
         self.taste = taste
 
     def __str__(self):
-        if self.taste:
-            return f"У вас газировка с {self.taste} вкусом"
-        else:
-            return "У вас обычная газировка"
+        return f"У вас газировка с {self.taste} вкусом" if self.taste else "У вас обычная газировка"
 
 
 soda = Soda()
