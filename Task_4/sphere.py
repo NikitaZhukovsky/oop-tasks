3fe006af72911c7784c024a53abbd0bd54ceccdb
NikitaZhukovsky <nikzuk2004@mail.ru>
2024-01-24 15:49:14 +0300

    New version

--- Task_4/sphere.py
+++ Task_4/sphere.py
@@ -4,12 +4,12 @@ import math
 class Sphere:
     def __init__(self, radius=1, x=0, y=0, z=0):
         self.radius = radius
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def get_info(self):
-        return self.radius, self.x, self.y, self.z
+        if all(isinstance(coord, (int, float)) for coord in (x, y, z)):
+            self.x = x
+            self.y = y
+            self.z = z
+        else:
+            raise ValueError("Координаты должны быть числами.")
 
     def get_volume(self):
         volume = (4/3) * math.pi * (self.radius ** 3)
@@ -20,31 +20,43 @@ class Sphere:
         return area
 
     def get_radius(self):
-        return self.radius
+        if isinstance(self.radius, (int, float)) and self.radius > 0:
+            return self.radius
+        else:
+            raise ValueError("Радиус должен быть положительным числом.")
 
     def get_center(self):
-        center = (self.x, self.y, self.z)
-        return center
+        return self.x, self.y, self.z
 
     def set_radius(self, radius):
-        self.radius = radius
+        if isinstance(radius, (int, float)) and radius > 0:
+            self.radius = radius
+        else:
+            raise ValueError("Радиус должен быть положительным числом.")
+
+    def set_center(self, x, y, z):
+        if all(isinstance(coord, (int, float)) for coord in (x, y, z)):
+            self.x = x
+            self.y = y
+            self.z = z
+        else:
+            raise ValueError("Координаты должны быть числами.")
 
     def is_point_inside(self, x, y, z):
-        distance = math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2 + (z - self.z) ** 2)
-        if distance <= self.radius:
-            return True
+        if all(isinstance(coord, (int, float)) for coord in (x, y, z)):
+            distance = math.sqrt((x - self.x) ** 2 + (y - self.y) ** 2 + (z - self.z) ** 2)
+            return True if distance <= self.radius else False
         else:
-            return False
+            raise ValueError("Координаты точки должны быть числами.")
 
 
-sphere = Sphere(5, 1, 2, 3)
-print(f"Параметры сферы: {sphere.get_info()}")
-print(f"Объем шара ограниченной текущей сферой: {sphere.get_volume()}")
+sphere = Sphere(10)
+print(f"Параметры сферы: радиус - {sphere.get_radius()}, центр - {sphere.get_center()}")
+print(f"Объем шара, ограниченного текущей сферой: {sphere.get_volume()}")
 print(f"Площадь внешней поверхности сферы: {sphere.get_square()}")
-print(f"Радиус текущей сферы: {sphere.get_radius()}")
-print(f"Центр сферы: {sphere.get_center()}")
 
-sphere.set_radius(10)
-print(f"Сфера с новым радиусом: {sphere.get_radius()}")
+sphere.set_radius(-10)
+sphere.set_center(2, 3, 4)
+print(f"Новые параметры сферы: радиус - {sphere.get_radius()}, центр - {sphere.get_center()}")
 
 print(f"Точка находится внутри сферы: {sphere.is_point_inside(5, 6, 8)}")
